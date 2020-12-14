ece3214960a5b31fdd95808297cec4faedbc84e3
Miguel del Mazo <migueldelmazo@gmail.com>
2020-12-14 11:42:38 +0100

    Added static server exercise

--- express-static-server/src/index.js
+++ express-static-server/src/index.js
@@ -2,31 +2,26 @@ const path = require('path');
 const express = require('express');
 const app = express();
 
-// api endpoints
+// create app server
+const serverPort = 3000;
+app.listen(serverPort, () => {
+  console.log(`App listening at http://localhost:${serverPort}`);
+});
 
-app.get('/users', (req, res) => {
+// listen GET > http://localhost:3000/api/
+app.get('/api/hello-world', (req, res) => {
   res.json({
     result: 'Hello World!'
   });
 });
 
-// express static server
-
+// config express static server
 const staticServerPath = './public'; // relative to the root of the project
 app.use(express.static(staticServerPath));
 
 // not found error
-
 app.get('*', (req, res) => {
   const notFoundFileRelativePath = '../public/404.html'; // relative to this directory
   const notFoundFileAbsolutePath = path.join(__dirname, notFoundFileRelativePath);
   res.status(404).sendFile(notFoundFileAbsolutePath);
-});
-
-// init express server
-
-const serverPort = 3000;
-
-app.listen(serverPort, () => {
-  console.log(`App listening at http://localhost:${serverPort}`);
 });
