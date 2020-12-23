ffa46ba86180eda0ff9255bc29a39221b8859fea
Miguel del Mazo <migueldelmazo@gmail.com>
2020-12-23 12:53:05 +0100

    Added node intro excecise

--- node-fs-read-file/index.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-
-fs.readFile('./input.txt', 'utf8', (err, fileContent) => {
-  if (err) {
-    console.log('Error:', err);
-  } else {
-    console.log(fileContent);
-  }
-});
