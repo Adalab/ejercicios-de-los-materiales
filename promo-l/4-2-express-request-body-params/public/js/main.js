af5e132758aafea167a17aefc18749ce01c5ae28
Miguel del Mazo <migueldelmazo@gmail.com>
2021-01-06 22:29:39 +0100

    Added body params lesson

--- promo-l/4-2-express-request-body-params/public/js/main.js
+++ promo-l/4-2-express-request-body-params/public/js/main.js
@@ -1,20 +1,36 @@
 'use strict';
 
-document.querySelector('.js-btn').addEventListener('click', () => {
+// post new user
+
+document.querySelector('.js-btn-post-new-user').addEventListener('click', () => {
   const inputName = document.querySelector('.js-input-name');
   const inputEmail = document.querySelector('.js-input-email');
 
-  const bodyParams = {
-    userName: inputName.value,
-    userEmail: inputEmail.value
-  };
+  // create query params
+  const queryParams = `?userName=${inputName.value}&userEmail=${inputEmail.value}`;
 
-  fetch('http://localhost:3000/new-user', {
-    method: 'POST',
-    body: JSON.stringify(bodyParams)
-  })
+  fetch('http://localhost:3000/user' + queryParams, { method: 'POST' })
     .then(response => response.json())
     .then(responseData => {
-      console.log('Data:', responseData);
+      console.log('Server response:', responseData);
+      printJson('.js-post-new-user-result', responseData);
     });
 });
+
+// get users data
+
+document.querySelector('.js-btn-get-users').addEventListener('click', () => {
+  fetch('http://localhost:3000/users')
+    .then(response => response.json())
+    .then(responseData => {
+      console.log('Server response:', responseData);
+      printJson('.js-get-users-result', responseData);
+    });
+});
+
+// helper
+
+const printJson = (selector, jsonData) => {
+  const jsonHtml = JSON.stringify(jsonData, null, 2);
+  document.querySelector(selector).innerHTML = jsonHtml;
+};
--- promo-l/4-2-express-request-body-params/src/index.js
+++ promo-l/4-2-express-request-body-params/src/index.js
@@ -1,31 +1,47 @@
 const express = require('express');
 const cors = require('cors');
 
-// create app server
-const app = express();
+// create server
+const server = express();
 
 // set express middleware
-app.use(express.json());
-app.use(cors());
+server.use(express.json());
+server.use(cors());
 
 // init express aplication
 const serverPort = 3000;
-app.listen(serverPort, () => {
-  console.log(`App listening at http://localhost:${serverPort}`);
+server.listen(serverPort, () => {
+  console.log(`Server listening at http://localhost:${serverPort}`);
 });
+
+// static server
+const staticServerPath = './public';
+server.use(express.static(staticServerPath));
+
+// users
+
+const users = []; // fake users data base
 
 // api endpoints
 
-app.post('/new-user', (req, res) => {
-  console.log('Body params:', req.body);
-  console.log('Body param userName:', req.body.userName);
-  console.log('Body param userEmail:', req.body.userEmail);
+server.post('/user', (req, res) => {
+  console.log('Query params:', req.query);
+  console.log('Query param userName:', req.query.userName);
+  console.log('Query param userEmail:', req.query.userEmail);
+
+  // add new user to daba base
+  users.push({
+    name: req.query.userName,
+    email: req.query.userEmail
+  });
 
   res.json({
     result: 'User created'
   });
 });
 
-// express static server
-
-app.use(express.static('./public'));
+server.get('/users', (req, res) => {
+  res.json({
+    result: users
+  });
+});
