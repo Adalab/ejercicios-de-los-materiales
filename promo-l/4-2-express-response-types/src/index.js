8c0251a6928e75797f9b63f7237ddb892b9a252c
Miguel del Mazo <migueldelmazo@gmail.com>
2021-01-07 20:12:23 +0100

    Added response types lesson

--- promo-l/4-2-express-response-types/src/index.js
+++ promo-l/4-2-express-response-types/src/index.js
@@ -1,6 +1,12 @@
-const path = require('path');
 const express = require('express');
+const cors = require('cors');
+
+// create server
 const app = express();
+
+// set express middleware
+app.use(express.json());
+app.use(cors());
 
 // create app server
 const serverPort = 3000;
@@ -18,25 +24,27 @@ app.get('/redirect-to-adalab', (req, res) => {
   res.redirect('https://adalab.es');
 });
 
-app.get('/response-with-file', (req, res) => {
-  res.sendFile(__dirname + '/assets/nodejs.jpg');
+app.get('/response-with-file-pdf', (req, res) => {
+  res.sendFile(__dirname + '/files/example.pdf');
 });
 
 app.get('/download-a-file', (req, res) => {
-  res.download(__dirname + '/assets/nodejs.jpg', 'logo.jpg');
+  res.download(__dirname + '/files/example.pdf', 'ejemplo.pdf');
 });
 
 app.get('/response-with-html', (req, res) => {
+  const date = new Date();
   const htmlCode = `
     <!DOCTYPE html>
     <html lang="en">
       <head>
         <title>Express</title>
-        <link rel="stylesheet" href="http://beta.adalab.es/resources/stylesheets/all.css" />
+        <link rel="stylesheet" href="http://beta.adalab.es/resources/stylesheets/all.css" >
       </head>
       <body class="page">
         <header>
           <h1>Express: Respondiendo con un HTML</h1>
+          <h2>Hoy es: ${date}</h2>
         </header>
       </body>
     </html>`;
@@ -45,19 +53,14 @@ app.get('/response-with-html', (req, res) => {
 
 app.get('/response-with-404-status-error', (req, res) => {
   // more info about http status codes: https://developer.mozilla.org/es/docs/Web/HTTP/Status
-  res.status(404).json({
-    errorCode: 'data-not-found',
-    errorMessage: 'Data not found'
-  });
-  // another way to do it is:
-  // res.status(404);
-  // res.json({
+  // res.status(404).json({
   //   errorCode: 'data-not-found',
   //   errorMessage: 'Data not found'
   // });
-});
-
-app.get('/response-twice', (req, res) => {
-  res.json({ result: 'first response' });
-  res.json({ result: 'second response' });
+  // another way to do it is:
+  res.status(404);
+  res.json({
+    errorCode: 'data-not-found',
+    errorMessage: 'Data not found'
+  });
 });
