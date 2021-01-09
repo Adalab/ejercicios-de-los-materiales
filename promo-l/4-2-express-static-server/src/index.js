adbaa4a47d9ecda58f4c24ef70814bd56c207328
Miguel del Mazo <migueldelmazo@gmail.com>
2021-01-09 18:23:56 +0100

    Added static server lesson

--- promo-l/4-2-express-static-server/src/index.js
+++ promo-l/4-2-express-static-server/src/index.js
@@ -8,20 +8,14 @@ app.listen(serverPort, () => {
   console.log(`App listening at http://localhost:${serverPort}`);
 });
 
-// listen GET > http://localhost:3000/api/
-app.get('/api/hello-world', (req, res) => {
-  res.json({
-    result: 'Hello World!'
-  });
-});
-
 // config express static server
 const staticServerPath = './public'; // relative to the root of the project
 app.use(express.static(staticServerPath));
 
 // not found error
 app.get('*', (req, res) => {
-  const notFoundFileRelativePath = '../public/404-not-found.html'; // relative to this directory
+  // relative to this directory
+  const notFoundFileRelativePath = '../public/404-not-found.html';
   const notFoundFileAbsolutePath = path.join(__dirname, notFoundFileRelativePath);
   res.status(404).sendFile(notFoundFileAbsolutePath);
 });
