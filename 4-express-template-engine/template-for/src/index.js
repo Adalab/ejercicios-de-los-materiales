5ab51349b4fe6cbe5d372b10ca31f82c585a65b8
Miguel del Mazo <migueldelmazo@gmail.com>
2021-06-10 14:33:42 +0200

    Moved to root folder

---
+++ 4-express-template-engine/template-for/src/index.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const cors = require('cors');
+const films = require('./films-data.json');
+
+// create app server
+const app = express();
+
+// set express middlewares
+app.use(express.json());
+app.use(cors());
+
+// set template engine middlewares
+app.set('view engine', 'ejs');
+
+// init express aplication
+const serverPort = 3000;
+app.listen(serverPort, () => {
+  console.log(`App listening at http://localhost:${serverPort}`);
+});
+
+// endpoints
+
+app.get('/es/film:filmId.html', (req, res) => {
+  // get film data
+  const filmData = films.find(film => film.id === req.params.filmId);
+  console.log('film data', filmData);
+
+  // response with rendered template
+  if (filmData) {
+    // ensure data
+    filmData.title = filmData.title || '';
+    filmData.year = filmData.year || '';
+    filmData.director = filmData.director || '';
+    filmData.country = filmData.country || '';
+    filmData.awards = filmData.awards || [];
+    res.render('pages/film', filmData);
+  } else {
+    res.render('pages/film-not-found');
+  }
+});
