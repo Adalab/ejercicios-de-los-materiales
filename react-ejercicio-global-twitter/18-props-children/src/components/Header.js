908f3ba2b8b1f7b2f8fe66768d01bbf2e96a4839
Miguel del Mazo <migueldelmazo@gmail.com>
2021-09-10 11:03:34 +0200

    Added Twitter exercise

---
+++ react-ejercicio-global-twitter/18-props-children/src/components/Header.js
@@ -0,0 +1,24 @@
+import HeaderMenuItem from './HeaderMenuItem';
+import HeaderMenuButton from './HeaderMenuButton';
+
+const Header = props => {
+  return (
+    <header className="header">
+      <nav className="menu">
+        <ul className="menu__items">
+          <HeaderMenuItem text="Ir al inicio" href="/home" liClass="twitter" />
+          <HeaderMenuItem text="Ir al inicio" href="/home" liClass="home" />
+          <HeaderMenuItem text="Buscar" href="/search" liClass="search" />
+          <HeaderMenuItem text="Perfil" href="/profile" liClass="profile" />
+          <HeaderMenuButton
+            text="Twittear"
+            liClass="tweet"
+            handleClick={props.handleToggleCompose}
+          />
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
