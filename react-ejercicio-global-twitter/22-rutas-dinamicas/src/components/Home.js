c0a881ff4fabdc23d839eda3ccf057f040c43127
Miguel del Mazo <migueldelmazo@gmail.com>
2021-09-27 10:27:51 +0200

    Added Twitter exercise

---
+++ react-ejercicio-global-twitter/22-rutas-dinamicas/src/components/Home.js
@@ -0,0 +1,13 @@
+import '../styles/components/Home.scss';
+
+const Home = () => {
+  return (
+    <section className="home">
+      <header>
+        <h1 className="header__title">Inicio</h1>
+      </header>
+    </section>
+  );
+};
+
+export default Home;
