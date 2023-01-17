ee06a88d0c8ff5ce2932eae467f8d861dedaf861
Yanelis Serrano <yaneliserranotaylor@gmail.com>
2023-01-17 09:00:35 +0100

    Update createRoot in React18

--- node-ejercicio-netflix/web/src/index.js
+++ node-ejercicio-netflix/web/src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import { HashRouter } from 'react-router-dom';
 import App from './components/App';
 import './stylesheets/index.scss';
 
-ReactDOM.render(
-  <HashRouter>
-    <App />
-  </HashRouter>,
-  document.querySelector('#root')
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </React.StrictMode>
 );
+
