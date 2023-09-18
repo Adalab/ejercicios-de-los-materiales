f8dfb720a6f17b476794dcacaacc897460cb78fe
Yanelis Serrano <yaneliserranotaylor@gmail.com>
2023-09-18 15:07:27 +0200

    Add new project with React+Vite

---
+++ my-react-app/src/components/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from '../assets/react.svg'
+import viteLogo from '/vite.svg'
+import '../styles/App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h1>Hola mundo</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
