4a96345317d09aadf5278a6528a315b1a41e674c
Iván (profe) <ivan@adalab.es>
2024-09-18 17:39:24 +0200

    Update React and Express versions.

--- node-ejercicio-netflix/web/src/main.jsx
+++ node-ejercicio-netflix/web/src/main.jsx
@@ -1,10 +1,13 @@
-import ReactDOM from "react-dom/client";
-import App from "./components/App";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { HashRouter } from 'react-router-dom';
+import App from './components/App.jsx';
 import "./styles/index.scss";
-import { HashRouter } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <HashRouter>
-    <App />
-  </HashRouter>
-);
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </React.StrictMode>,
+)
